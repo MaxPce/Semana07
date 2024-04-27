2c68f6aa52bfa449e1643f0b5e304fba283d31f7
Alumno <Alumno@LAB1504-32>
2024-04-27 12:56:01 -0500

    Agregar archivos de proyecto.

---
+++ Business/ProductBusiness.cs
@@ -0,0 +1,41 @@
+﻿using Data;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class ProductBusiness
+    {
+        public List<Product> GetProducts()
+        {
+            List<Product> products = new List<Product>();
+            ProductData data = new ProductData();
+            products = data.GetProducts();
+            return products;
+        }
+
+        public List<Product> GetProductsInStock()
+        {
+            List<Product> products = new List<Product>();
+            ProductData data = new ProductData();
+            products = data.GetProducts();
+
+            var result = products.Where(x => x.Stock > 0 && x.Active).ToList();
+            return result;
+        }
+
+        public List<Product> GetProductsByName(string name)
+        {
+            List<Product> products = new List<Product>();
+            ProductData data = new ProductData();
+            products = data.GetProducts();
+
+            var result = products.Where(x => x.Name.Contains(name) && x.Active).ToList();
+            return result;
+        }
+    }
+}
---
+++ Data/ProductData.cs
@@ -0,0 +1,50 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Entity;
+
+
+namespace Data
+{
+    public class ProductData
+    {
+        public List<Product> GetProducts()
+        {
+            List<Product> products = new List<Product>();
+
+            string connectionString = "Data Source=tuServidor;Initial Catalog=tuBaseDeDatos;User ID=usuario;Password=contraseña";
+
+            string query = "SELECT Product_Id, Name, Price, Stock, Active FROM Products";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Product product = new Product
+                            {
+                                ProductId = Convert.ToInt32(reader["Product_Id"]),
+                                Name = reader["Name"].ToString(),
+                                Price = Convert.ToDecimal(reader["Price"]),
+                                Stock = Convert.ToInt32(reader["Stock"]),
+                                Active = Convert.ToBoolean(reader["Active"])
+                            };
+                            products.Add(product);
+                        }
+                    }
+                }
+            }
+
+            return products;
+        }
+    }
+}
+
